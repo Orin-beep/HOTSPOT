f614f23dc39d2449a197919821147b904f1d321c
Yongxin Ji <yxjijms@Gmail.com>
2022-11-12 16:09:04 +0800

    Update prepare_db.sh

--- prepare_db.sh
+++ prepare_db.sh
@@ -1,3 +1,3 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pvFZ6HWPmdAA0ZSOXS8xK4IFgkJXU7sA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1pvFZ6HWPmdAA0ZSOXS8xK4IFgkJXU7sA" -O database.tgz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q_1n8DyCU1GjDIOESuEqWbRV10R6fO5y' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1q_1n8DyCU1GjDIOESuEqWbRV10R6fO5y" -O database.tgz && rm -rf /tmp/cookies.txt
 tar -zxvf database.tgz
 rm database.tgz
