3361a4debcd348c4606c28db2e62d4b3981f2058
Yongxin Ji <yxjijms@Gmail.com>
2023-03-07 13:26:45 +0800

    Delete prepare_db.sh

--- prepare_db.sh
+++
@@ -1,3 +0,0 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q_1n8DyCU1GjDIOESuEqWbRV10R6fO5y' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1q_1n8DyCU1GjDIOESuEqWbRV10R6fO5y" -O database.tgz && rm -rf /tmp/cookies.txt
-tar -zxvf database.tgz
-rm database.tgz
