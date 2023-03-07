b9585cfd63ce7d9684ee1fa4c3765c05770769a3
Yongxin Ji <yxjijms@Gmail.com>
2023-03-07 13:27:17 +0800

    Add files via upload

---
+++ prepare_mdl.sh
@@ -0,0 +1,3 @@
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L6ogZhdAWJ7Ns8Hz59m7W2kFPMcMTC6u' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L6ogZhdAWJ7Ns8Hz59m7W2kFPMcMTC6u" -O database.tgz && rm -rf /tmp/cookies.txt
+tar -zxvf database.tgz
+rm database.tgz
