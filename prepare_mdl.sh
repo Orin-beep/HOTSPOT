f458b5c91a794e8e93c65d66797a20ad55170928
Yongxin Ji <yxjijms@Gmail.com>
2023-03-11 16:12:02 +0800

    Update prepare_mdl.sh

--- prepare_mdl.sh
+++ prepare_mdl.sh
@@ -1,3 +1,3 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L6ogZhdAWJ7Ns8Hz59m7W2kFPMcMTC6u' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L6ogZhdAWJ7Ns8Hz59m7W2kFPMcMTC6u" -O database.tgz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L6ogZhdAWJ7Ns8Hz59m7W2kFPMcMTC6u' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L6ogZhdAWJ7Ns8Hz59m7W2kFPMcMTC6u" -O models.tgz && rm -rf /tmp/cookies.txt
 tar -zxvf models.tgz
 rm models.tgz
